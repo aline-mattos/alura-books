674a8a7d36f5ed9db19f6ad00addda3e5ce40a77
aline-mattos <abelomo95@gmail.com>
2023-07-05 07:12:40 -0300

    creating header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import logo from './images/logo.svg';
+import Logo from './components/logo/';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt='logo'></img>
-          <p><strong>Alura</strong>Books</p>
-        </div>
+        <Logo></Logo>
       </header>
     </div>
   );
